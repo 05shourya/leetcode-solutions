69ae812372636a6770c550c7b0bfaedb1ca5ac9d
Shourya Gupta <61070498+05shourya@users.noreply.github.com>
2025-04-02 20:47:28 +0530

    Create file

---
+++ 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.java
@@ -0,0 +1,16 @@
+// Last updated: 4/2/2025, 8:47:25 PM
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long maxTriplet = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int k = nums.length - 1; k > i; k--) {
+                int j = i + 1;
+                while (j < k) {
+                    maxTriplet = Math.max(maxTriplet, (long)(nums[i] - nums[j]) * nums[k]);
+                    j++;
+                }
+            }
+        }
+        return Math.max(0, maxTriplet);
+    }
+}
