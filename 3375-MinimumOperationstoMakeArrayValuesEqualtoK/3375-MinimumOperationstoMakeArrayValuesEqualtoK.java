2687fadaae341d771b091236fef06de1005752b5
Shourya Gupta <61070498+05shourya@users.noreply.github.com>
2025-04-09 14:18:59 +0530

    File updated at 4/9/2025, 2:18:59 PM

--- 3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.java
+++ 3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.java
@@ -1,25 +1,12 @@
-// Last updated: 4/9/2025, 2:13:52 PM
+// Last updated: 4/9/2025, 2:18:59 PM
 class Solution {
     public int minOperations(int[] nums, int k) {
-        Queue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
-        Set<Integer> seen = new HashSet<>();  
-        
+        Set<Integer> set = new HashSet<>(); 
         for(int num : nums){
             if(num < k) return -1; 
-            if(seen.add(num)){
-                q.add(num); 
-            }
+            else if (num > k) set.add(num);  
         }
 
-        int count = 0; 
-
-        while(!q.isEmpty()){
-            int val = q.poll(); 
-            if(val != k){
-                count++; 
-            }
-        }
-
-        return count; 
+        return set.size(); 
     }
 }
