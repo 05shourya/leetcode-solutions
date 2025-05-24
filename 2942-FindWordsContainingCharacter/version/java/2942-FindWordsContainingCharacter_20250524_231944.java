c68ec95fd872c8914467e0ea2956f121038a7597
Shourya Gupta <61070498+05shourya@users.noreply.github.com>
2025-05-24 23:19:44 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ 2942-FindWordsContainingCharacter/version/java/2942-FindWordsContainingCharacter_20250524_231944.java
@@ -0,0 +1,21 @@
+// Last updated: 5/24/2025, 11:19:44 PM
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>(); 
+        for(int i = 0 ; i < words.length ; i++){
+            if(isCharPresent(words[i] , x)){
+                ans.add(i); 
+            }
+        }
+        return ans; 
+    }
+
+    private boolean isCharPresent(String word , char x){
+        for(int i = 0 ; i < word.length() ; i++){
+            if(word.charAt(i) == x){
+                return true; 
+            }
+        }
+        return false;
+    }
+}
