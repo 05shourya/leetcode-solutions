9a82de84fd47a5270c8e5f9ac2dace8bfff29896
Shourya Gupta <61070498+05shourya@users.noreply.github.com>
2025-07-01 23:02:48 +0530

    Create 3483-AlternatingGroupsIi

---
+++ 3483-AlternatingGroupsIi/3483-AlternatingGroupsIi.java
@@ -0,0 +1,20 @@
+// Last updated: 7/1/2025, 11:02:48 PM
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n = colors.length;
+        int count = 0;
+        int left = 0;
+        
+        for (int right = 0; right < n + k - 1; right++) {
+            if (right > 0 && colors[right % n] == colors[(right - 1) % n]) {
+                left = right;  
+            }
+            
+            if (right - left + 1 >= k) {
+                count++;  
+            }
+        }
+        
+        return count;
+    }
+}
