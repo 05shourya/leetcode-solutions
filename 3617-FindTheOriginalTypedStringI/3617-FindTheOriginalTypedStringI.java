a4381d0efb22e1986a7563713956b1dcd623ee9b
Shourya Gupta <61070498+05shourya@users.noreply.github.com>
2025-07-01 23:02:39 +0530

    Create 3617-FindTheOriginalTypedStringI

---
+++ 3617-FindTheOriginalTypedStringI/3617-FindTheOriginalTypedStringI.java
@@ -0,0 +1,18 @@
+// Last updated: 7/1/2025, 11:02:39 PM
+class Solution {
+    public int possibleStringCount(String word) {
+        if(word.length() == 1){
+            return 1; 
+        }
+        int res = 1; 
+
+        for(int i = 1 ; i < word.length(); i++){
+            if(word.charAt(i) == word.charAt(i - 1)){
+                res++; 
+            }
+
+        }
+
+        return res; 
+    }
+}
