d9c0ad77bc4b561268ea71f3bb905eb1ea990fbf
Shourya Gupta <61070498+05shourya@users.noreply.github.com>
2025-07-01 23:04:01 +0530

    Create 2805-ArrayWrapper

---
+++ 2805-ArrayWrapper/2805-ArrayWrapper.js
@@ -0,0 +1,30 @@
+// Last updated: 7/1/2025, 11:04:01 PM
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums; 
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((acc, val) => acc + val, 0);
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+     return `[${this.nums.join(',')}]`;
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
